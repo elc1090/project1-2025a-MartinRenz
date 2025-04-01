8f8e7e2e0aa277bfe206cd94f0606283a41f188f
MartinRenz <martinrenz@outlook.com>
2025-03-31 21:10:48 -0300

    - Adiciona o ícone da Udemy. - Altera o título da página.

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function Course() {
+  useEffect(() => {
+    document.title = 'Algoritmos e Lógica de Programação - O Curso COMPLETO';
+  }, []);
+
   return (
     <div>
       <h1>Hello world!</h1>
