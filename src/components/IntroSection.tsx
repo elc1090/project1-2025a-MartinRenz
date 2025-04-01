4ee6ca98cfd2f54f29ef5bda8f6efd119c8803ae
MartinRenz <martinrenz@outlook.com>
2025-04-01 00:52:43 -0300

    - ALterações IntroSection

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -10,7 +10,12 @@ const IntroSection = () => {
           O curso definitivo de Lógica de Programação. Do VisualG às principais
           linguagens do mercado: C, C++, Python, C# e Java
         </h3>
+        <div className="classification-container">
+          <p className="classification"><strong>Classificação mais alta</strong></p>
+          <p className="students-count">108.761 alunos</p>
+        </div>
         <p>Criado por Nelio Alves</p>
+        <p>Última atualização em 06/2024 Português Português [Automático]</p>
       </div>
     </section>
   );
